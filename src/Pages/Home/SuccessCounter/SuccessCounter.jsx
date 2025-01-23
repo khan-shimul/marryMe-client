7f77cd75b80a3d7d612cd340f2c968ba07e453cc
khan-shimul <khanshimul196@gmail.com>
2025-01-24 00:08:10 +0600

    stast-update

--- src/Pages/Home/SuccessCounter/SuccessCounter.jsx
+++ src/Pages/Home/SuccessCounter/SuccessCounter.jsx
@@ -1,6 +1,8 @@
 import img1 from "../../../assets/images/home/prize.png";
 import img2 from "../../../assets/images/home/trust.png";
 import img3 from "../../../assets/images/home/rings.png";
+import { MdFavorite } from "react-icons/md";
+import { FaFemale, FaMale, FaUsers } from "react-icons/fa";
 
 const SuccessCounter = () => {
   return (
@@ -16,70 +18,68 @@ const SuccessCounter = () => {
           Celebrating Connections and Success Stories
         </h6>
       </div>
-      {/* Stats - Todo: stats needs to be dynamically from db */}
+      {/* Todo: stats needs to be dynamically from db */}
       <div className="mx-5 md:mx-16 -mt-14">
-        <div className="stats shadow w-full">
+        <div className="stats py-4 px-3 shadow w-full">
           <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
+            <div className="flex items-center gap-x-10">
+              <div className="border border-[#aa8557] rounded p-2">
+                <MdFavorite className="inline-block h-7 w-7 text-[#66451C]" />
+              </div>
+              <div>
+                <div className="font-cinzel text-4xl font-bold text-[#66451C]">
+                  2K
+                </div>
+                <div className="stat-title uppercase text-[#66451C] mt-2 text-sm font-poppins">
+                  Couples pared
+                </div>
+              </div>
             </div>
-            <div className="stat-title">Total Biodata</div>
-            <div className="stat-value">31K</div>
-            <div className="stat-desc">Jan 1st - Feb 1st</div>
           </div>
-
           <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                ></path>
-              </svg>
+            <div className="flex items-center gap-x-10">
+              <div className="border border-[#aa8557] rounded p-2">
+                <FaUsers className="inline-block h-7 w-7 text-[#66451C]" />
+              </div>
+              <div>
+                <div className="font-cinzel text-4xl font-bold text-[#66451C]">
+                  4000+
+                </div>
+                <div className="stat-title uppercase text-[#66451C] mt-2 text-sm font-poppins">
+                  Registered
+                </div>
+              </div>
             </div>
-            <div className="stat-title">New Users</div>
-            <div className="stat-value">4,200</div>
-            <div className="stat-desc">↗︎ 400 (22%)</div>
           </div>
-
           <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                ></path>
-              </svg>
+            <div className="flex items-center gap-x-10">
+              <div className="border border-[#aa8557] rounded p-2">
+                <FaMale className="inline-block h-7 w-7 text-[#66451C]" />
+              </div>
+              <div>
+                <div className="font-cinzel text-4xl font-bold text-[#66451C]">
+                  1600+
+                </div>
+                <div className="stat-title uppercase text-[#66451C] mt-2 text-sm font-poppins">
+                  Boys
+                </div>
+              </div>
             </div>
-            <div className="stat-title">New Registers</div>
-            <div className="stat-value">1,200</div>
-            <div className="stat-desc">↘︎ 90 (14%)</div>
+          </div>
+          <div className="stat">
+            <div className="flex items-center gap-x-10">
+              <div className="border border-[#aa8557] rounded p-2">
+                <FaFemale className="inline-block h-7 w-7 text-[#66451C]" />
+              </div>
+              <div>
+                <div className="font-cinzel text-4xl font-bold text-[#66451C]">
+                  2000+
+                </div>
+                <div className="stat-title uppercase text-[#66451C] mt-2 text-sm font-poppins">
+                  Girls
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
