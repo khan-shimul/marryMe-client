e7960863c639dc33c85997ea2797ed0ab4232a3b
khan-shimul <khanshimul196@gmail.com>
2025-03-05 00:07:48 +0600

    added-footer

--- src/Pages/Home/SuccessStory/SuccessCard.jsx
+++ src/Pages/Home/SuccessStory/SuccessCard.jsx
@@ -1,13 +1,21 @@
 import PropTypes from "prop-types";
 
 const SuccessCard = ({ review }) => {
-  const { image, successStory } = review;
+  const { image, successStory, name, marriageDate } = review;
   return (
     <div className="card border bg-[#FEFCF7] relative">
       <div className="card-body">
-        <div className="avatar">
-          <div className="w-16 rounded-full">
-            <img src={image} />
+        <div className="flex items-center gap-x-4">
+          <div className="avatar">
+            <div className="w-16 rounded-full">
+              <img src={image} />
+            </div>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold text-[#66451C] font-cinzel">
+              {name}
+            </h3>
+            <h5 className="text-gray-500 font-medium">{marriageDate}</h5>
           </div>
         </div>
         <div>{/* Name & Rating */}</div>
