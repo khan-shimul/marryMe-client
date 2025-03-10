bf3deddfd5dc64c07a1cfcfee7bf3bda0ec8fb59
khan-shimul <khanshimul196@gmail.com>
2025-03-11 00:47:35 +0600

    implement-react-rating

--- src/Pages/Home/SuccessStory/SuccessCard.jsx
+++ src/Pages/Home/SuccessStory/SuccessCard.jsx
@@ -1,7 +1,10 @@
+import { Rating } from "@smastrom/react-rating";
 import PropTypes from "prop-types";
+import "@smastrom/react-rating/style.css";
 
 const SuccessCard = ({ review }) => {
-  const { image, successStory, name, marriageDate } = review;
+  const { image, successStory, name, marriageDate, rating } = review;
+  console.log(review.rating);
   return (
     <div className="card border bg-[#FEFCF7] relative">
       <div className="card-body">
@@ -15,11 +18,13 @@ const SuccessCard = ({ review }) => {
             <h3 className="text-lg font-semibold text-[#66451C] font-cinzel">
               {name}
             </h3>
-            <h5 className="text-gray-500 font-medium">{marriageDate}</h5>
+            <div className="flex items-center">
+              <Rating style={{ maxWidth: 80 }} value={rating} readOnly />
+            </div>
           </div>
         </div>
-        <div>{/* Name & Rating */}</div>
-        <p className="text-[#66451C]">{successStory}</p>
+        <p className="text-[#66451C] font-poppins text-base">{successStory}</p>
+        <h5 className="text-gray-500 font-medium">{marriageDate}</h5>
       </div>
     </div>
   );
